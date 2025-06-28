ef057945c71ac548b2fd4ff5fafcdb8e06ecc7fb
Hailer367 <brookwondwossen442@gmail.com>
2025-06-28 08:44:43 -0700

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./lib/browser-polyfills";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
